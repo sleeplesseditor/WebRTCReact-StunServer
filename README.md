a107ab75bcfc2b137cf39dc252d53f685f6ff59f
sleeplesseditor <josh@bryson.me.uk>
2026-07-22 16:41:38 +0100

    Fixed scripts

--- README.md
+++ README.md
@@ -9,7 +9,8 @@
 An initial investigation into the use of Stun Servers with WebRTC, with an updated version of the first coursework project for [Robert Bunch's Udemy course](https://www.udemy.com/course/mastering-webrtc-part-2-real-time-video-and-screen-share).
 
 ## Running the App Locally
-- Run `npm install` in the root project folder
+- CD into `server` folder and run `npm install`
+- Ensure you have `mkcert` installed globally and run `mkcert create-ca`, followed by `mkcert create-cert`
 - CD into `client` folder
 - Run `npm install`
 - Return to root project folder and run `npm run dev`
--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const https = require('https');
 const express = require('express');
 const socketIO = require('socket.io');
@@ -5,7 +6,22 @@ const app = express();
 
 app.use(express.static(__dirname));
 
-const expressServer = https.createServer({}, app);
+const fsKey = fs.readFileSync('cert.key');
+const fsCert = fs.readFileSync('cert.crt');
+
+const expressServer = https.createServer({fsKey, fsCert}, app);
 const io = socketIO(expressServer)
 
 expressServer.listen(8181);
+
+const offers = {
+
+};
+
+socketIO.on('connection', (socket) => {
+    console.log(`Connection established on Socket ${socket}`);
+
+    socket.on('newOffer', () => {
+
+    });
+});
